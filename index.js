9099f1d55a069ddda78d2ad0e80d5e4bf55a5e3a
Vineet-Sharma29 <vineet.s17@iiits.in>
2020-04-17 18:02:01 +0530

    added cookies and htmlrewriter

--- index.js
+++ index.js
@@ -2,16 +2,52 @@ addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
 
-async function getVariants(urls) {
+class handleTitle {
+  element(e) {
+    e.prepend("Hey There!!")
+  }
+}
+
+class handleHeading {
+  element(e) {
+    e.prepend("This is ")
+  }
+}
+
+
+class handleText {
+  element(e) {
+    e.setInnerContent("Stay Safe. Stay Home. Stay Strong. Everthing's gonna be alright !!")
+  }
+}
+
+class handleURL {
+  element(e) {
+    e.setAttribute('href', 'https://sourcerer.io/vineet-sharma29')
+    e.setInnerContent('View My Sourcerer Profile')
+  }
+}
+
+const rewriter = new HTMLRewriter()
+  .on('title', new handleTitle())
+  .on('h1#title', new handleHeading())
+  .on('p#description', new handleText())
+  .on('a#url', new handleURL())
+
+
+async function getVariants(urls, cookie) {
 
   let variant;
-  variant = Math.random() < 0.5 ? 1 : 2;
+
+  variant = cookie ? cookie.includes(`variant=1`) ? 1 : 2 : Math.random() < 0.5 ? 1 : 2;
 
   const page = await fetch(urls[variant - 1], { headers: { 'Content-Type': 'text/html' } })
 
-  const response = new Response(page.body, {
+  const response = new Response(rewriter.transform(page).body, {
     headers: { 'Content-Type': 'text/html' }
   })
+
+  response.headers.set('Set-Cookie', `variant=${variant}; Expires=Fri, 17 Jul 2020 00:00:00 GMT; Path='/';`)
 
   return response
 }
@@ -19,12 +55,14 @@ async function getVariants(urls) {
 
 async function handleRequest(request) {
 
+  const cookie = request.headers.get('cookie')
+
   let url = 'https://cfw-takehome.developers.workers.dev/api/variants'
   const variants = await fetch(url, {
     headers: { 'Content-Type': 'text/json' },
   })
   const urls = await variants.json()
 
-  return getVariants(urls.variants)
+  return getVariants(urls.variants, cookie)
 }
 
